7471977ed660aefd503ba47837cef2bbf26d54f3
Maggie Pierse <mpierse@udel.edu>
2019-09-30 21:13:12 -0400

    day 1 game improvements

--- ruby/day-one-game.rb
+++ ruby/day-one-game.rb
@@ -1,12 +1,22 @@
 #!/usr/bin/env ruby
 
+#print "hello world"
+puts "hello world"
+
+# find the index of the word Ruby in "Hello Ruby"
+puts "Hello Ruby".index "Ruby"
+
+#print "Maggie" ten times
+(1...10).each {puts "Maggie"}
+
+(1...10).each {|x| puts "this is sentence " << x}
+
+#Make a number guessing game
 num = rand(10)
-loop do
-var = gets 
-	if var.to_i > num || var.to_i < num
-	puts "WRONG"
-	else var.to_i == num
-	puts "You win!"
-	break
-	end
+var = -1
+while var != num do
+	var = gets.to_i
+	puts "WRONG" if var > num || var < num 
 end
+
+puts "YOU WIN"
